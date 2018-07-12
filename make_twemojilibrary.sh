3b2b42d98ff1b4a4fe0e734dde5819059b8e9cc2
Vinícius Pontin <vpontin93@gmail.com>
2018-07-11 22:08:04 -0300

    b

--- make_twemojilibrary.sh
+++ make_twemojilibrary.sh
@@ -13,13 +13,13 @@ set temp_dir=$script_dir/tmp
 # The dir where the finished icons are stored
 set final_dir=$script_dir/bin/library
 
-mkdir -p $final_dir
+mkdir $final_dir
 
 for f in $emoji_dir
 do
 	echo "Processing $f file..."
 	# Pasta temporária
-	mkdir -p $temp_dir/${f%.*}
+	mkdir $temp_dir/${f%.*}
 	cd $temp_dir/${f%.*}
 	
 	# Processa svg do emoji

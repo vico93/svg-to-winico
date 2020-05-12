c282ca50c8321bb3b0703fb5d56e9f7a6eb4c60a
Vico <vpontin93@gmail.com>
2020-05-12 09:45:43 -0300

    Adicionado quotes em volta dos nomes de arquivos
    Sou burro e esqueci da possibilidade de ter .svgs com espaço no nome

--- make_twemojifolder.sh
+++ make_twemojifolder.sh
@@ -16,29 +16,29 @@ do
 	echo "Processing $(basename "${f%.*}") file..."
 	
 	# Take action on each file. $f store current file name
-	mkdir -p $TMP_FOLDER/$(basename "${f%.*}")
-	cd $TMP_FOLDER/$(basename "${f%.*}")
+	mkdir -p "$TMP_FOLDER/$(basename "${f%.*}")"
+	cd "$TMP_FOLDER/$(basename "${f%.*}")"
 	
-	rsvg-convert $f -w 120 -h 120 -o emoji_256.png
-	rsvg-convert $f -w 32 -h 32 -o emoji_64.png
-	rsvg-convert $f -w 24 -h 24 -o emoji_48.png
-	rsvg-convert $f -w 20 -h 20 -o emoji_40.png
-	rsvg-convert $f -w 16 -h 16 -o emoji_32.png
-	rsvg-convert $f -w 12 -h 12 -o emoji_24.png
-	rsvg-convert $f -w 10 -h 10 -o emoji_20.png
+	rsvg-convert "$f" -w 120 -h 120 -o emoji_256.png
+	rsvg-convert "$f" -w 32 -h 32 -o emoji_64.png
+	rsvg-convert "$f" -w 24 -h 24 -o emoji_48.png
+	rsvg-convert "$f" -w 20 -h 20 -o emoji_40.png
+	rsvg-convert "$f" -w 16 -h 16 -o emoji_32.png
+	rsvg-convert "$f" -w 12 -h 12 -o emoji_24.png
+	rsvg-convert "$f" -w 10 -h 10 -o emoji_20.png
 	
 	# Composite the folder icon
-	composite -gravity SouthEast -geometry +16+29 emoji_256.png $SRC_FOLDER/256.png compositeicon_256.png
-	composite -gravity SouthEast -geometry +4+6 emoji_64.png $SRC_FOLDER/64.png compositeicon_64.png
-	composite -gravity SouthEast -geometry +3+6 emoji_48.png $SRC_FOLDER/48.png compositeicon_48.png
-	composite -gravity SouthEast -geometry +2+5 emoji_40.png $SRC_FOLDER/40.png compositeicon_40.png
-	composite -gravity SouthEast -geometry +3+3 emoji_32.png $SRC_FOLDER/32.png compositeicon_32.png
-	composite -gravity SouthEast -geometry +1+3 emoji_24.png $SRC_FOLDER/24.png compositeicon_24.png
-	composite -gravity SouthEast -geometry +2+2 emoji_20.png $SRC_FOLDER/20.png compositeicon_20.png
+	composite -gravity SouthEast -geometry +16+29 emoji_256.png "$SRC_FOLDER/256.png" compositeicon_256.png
+	composite -gravity SouthEast -geometry +4+6 emoji_64.png "$SRC_FOLDER/64.png" compositeicon_64.png
+	composite -gravity SouthEast -geometry +3+6 emoji_48.png "$SRC_FOLDER/48.png" compositeicon_48.png
+	composite -gravity SouthEast -geometry +2+5 emoji_40.png "$SRC_FOLDER/40.png" compositeicon_40.png
+	composite -gravity SouthEast -geometry +3+3 emoji_32.png "$SRC_FOLDER/32.png" compositeicon_32.png
+	composite -gravity SouthEast -geometry +1+3 emoji_24.png "$SRC_FOLDER/24.png" compositeicon_24.png
+	composite -gravity SouthEast -geometry +2+2 emoji_20.png "$SRC_FOLDER/20.png" compositeicon_20.png
 	
 	# Make the .ico	
 	convert compositeicon_256.png compositeicon_64.png compositeicon_48.png compositeicon_40.png compositeicon_32.png compositeicon_24.png compositeicon_20.png emoji_32.png "$DIST_FOLDER/$(basename "${f%.*}").ico"
 	
 	cd ../../../
-	rm -rf $TMP_FOLDER/$(basename "${f%.*}")
+	rm -rf "$TMP_FOLDER/$(basename "${f%.*}")"
 done
--- make_twemojionly.sh
+++ make_twemojionly.sh
@@ -15,22 +15,22 @@ do
 	echo "Processing $(basename "${f%.*}") file..."
 	
 	# Take action on each file. $f store current file name
-	mkdir -p $TMP_FOLDER/$(basename "${f%.*}")
-	cd $TMP_FOLDER/$(basename "${f%.*}")
+	mkdir -p "$TMP_FOLDER/$(basename "${f%.*}")"
+	cd "$TMP_FOLDER/$(basename "${f%.*}")"
 	
-	rsvg-convert $f -w 256 -h 256 -o emoji_256.png 
-	rsvg-convert $f -w 96 -h 96 -o emoji_96.png
-	rsvg-convert $f -w 64 -h 64 -o emoji_64.png
-	rsvg-convert $f -w 48 -h 48 -o emoji_48.png
-	rsvg-convert $f -w 40 -h 40 -o emoji_40.png
-	rsvg-convert $f -w 32 -h 32 -o emoji_32.png
-	rsvg-convert $f -w 24 -h 24 -o emoji_24.png
-	rsvg-convert $f -w 20 -h 20 -o emoji_20.png
-	rsvg-convert $f -w 16 -h 16 -o emoji_16.png
+	rsvg-convert "$f" -w 256 -h 256 -o emoji_256.png 
+	rsvg-convert "$f" -w 96 -h 96 -o emoji_96.png
+	rsvg-convert "$f" -w 64 -h 64 -o emoji_64.png
+	rsvg-convert "$f" -w 48 -h 48 -o emoji_48.png
+	rsvg-convert "$f" -w 40 -h 40 -o emoji_40.png
+	rsvg-convert "$f" -w 32 -h 32 -o emoji_32.png
+	rsvg-convert "$f" -w 24 -h 24 -o emoji_24.png
+	rsvg-convert "$f" -w 20 -h 20 -o emoji_20.png
+	rsvg-convert "$f" -w 16 -h 16 -o emoji_16.png
 	
 	# Make the .ico	
 	convert emoji_256.png emoji_96.png emoji_64.png emoji_48.png emoji_40.png emoji_32.png emoji_24.png emoji_20.png emoji_16.png "$DIST_FOLDER/$(basename "${f%.*}").ico"
 	
 	cd ../../../
-	rm -rf $TMP_FOLDER/$(basename "${f%.*}")
+	rm -rf "$TMP_FOLDER/$(basename "${f%.*}")"
 done
